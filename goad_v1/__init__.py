6f68872b947a4db0595b3436570ed5a82d390cbb
shamgom <shamgom@hotmail.com>
2025-12-12 20:13:51 +0100

    Update version to 1.0.2 in __init__.py

--- goad_v1/__init__.py
+++ goad_v1/__init__.py
@@ -1,9 +1,9 @@
 """
-GOAD v5.0 - Global Optimization with ASE Design
+GOAD v1.0 - Global Optimization with ASE Design
 Advanced surface and molecule analysis with layer control
 """
 
-__version__ = "5.0.0"
+__version__ = "1.0.2"
 __author__ = "GOAD Team"
 
 from .analysis.surface_analyzer import SurfaceAnalyzer
